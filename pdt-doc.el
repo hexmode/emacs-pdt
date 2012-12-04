44e20326a4ff3d7bf08c2e4bca8e2c3dc84cd6ca
Marcin Antczak <marcin.antczak@neutrico.pl>
2012-12-04 04:43:16 +0100

    Added PDT doc module - based on phpplus-mode

---
+++ pdt-doc.el
@@ -0,0 +1,36 @@
+;;; pdt.el --- PHP Development Tools for GNU Emacs
+;;
+;; Copyright (C) 2012 neutrico.pl
+;;
+;; Name: Emacs PDT
+;; Author: Marcin Antczak <marcin.antczak@neutrico.pl>
+;; Version: 0.0.1
+;; Keywords: php
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+;;
+;;; Commentary:
+;;
+;; This package provides tolls for PHP development.
+;;
+;;; Code:
+
+(defgroup pdt-doc nil
+  "Customizations for PDT doc."
+  :group 'pdt
+  :prefix "pdt-doc-")
+
+(defcustom pdt-doc-default-author (cons "" "")
+  "Default author to insert in doc blocks."
+  :group 'pdt-doc
+  :type '(cons (string :tag "Name") (string :tag "Email Address")))
+
+(provide 'pdt-doc)
